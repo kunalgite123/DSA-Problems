750d39f53bcc080212ef21afd7f57132ae426124
Kunal Gite <kunalgite6677@gmail.com>
2026-02-28 21:55:03 +0530

    Add intersection function for two integer arrays
    Implemented a function to find the intersection of two arrays.

---
+++ Arrays/intersection.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2){
+       
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+
+    vector<int> ans;
+
+    int i = 0;
+    int j = 0;
+
+    while(i < nums1.size() && j < nums2.size()){
+        if(nums1[i] < nums2[j]){
+            i++;
+        }
+        else if(nums2[j] < nums1[i]){
+            j++;
+        }
+        else{
+            if(ans.empty() || ans.back() != nums1[i])
+                ans.push_back(nums1[i]);
+            i++; 
+            j++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> nums1 = {1,2,2,1};
+    vector<int> nums2 = {2,2};
+
+    vector<int> result = intersection(nums1, nums2);
+
+    cout << "Intersection: ";
+    for(int num : result){
+        cout << num << " ";
+    }
+
+    return 0;
+}
