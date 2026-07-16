32b2c2855c329da1796c09e4b5caaed8971e03bd
Kunal Gite <kunalgite6677@gmail.com>
2026-07-16 09:28:03 +0530

    Add function to delete nodes from linked list
    Implement modifiedList function to remove nodes with values in the given array.

---
+++ Linklist/deletenodearray.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int>s(begin(nums),end(nums));
+
+        while(head!=NULL && s.find(head->val)!=s.end()){
+            ListNode* temp = head;
+            head = head->next;
+            
+        }
+
+
+        ListNode* curr = head;
+
+        while(curr!=NULL && curr->next!=NULL){
+            if(s.find(curr->next->val)!=s.end()){
+                ListNode* dub = curr->next;
+                curr->next = curr->next->next;
+                
+            }
+            else{
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
